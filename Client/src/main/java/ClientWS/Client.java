7a8eb058ff05e46b5d6dc608af1dc3b9b45a145d
Sinica15 <ruja_vl@mail.ru>
2019-04-23 12:31:58 +0300

    modif client on ws

--- Client/src/main/java/ClientWS/Client.java
+++ Client/src/main/java/ClientWS/Client.java
@@ -1,32 +1,28 @@
 package ClientWS;
 
+import javax.websocket.ContainerProvider;
+import javax.websocket.DeploymentException;
+import javax.websocket.WebSocketContainer;
+import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Client {
 
+    final static CountDownLatch messageLatch = new CountDownLatch(1);
+
     public static void main(String[] args) {
         try {
-            // open websocket
-            final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(new URI("ws://localhost:9003/chat"));
-
-            // add listener
-            clientEndPoint.addMessageHandler(new WebsocketClientEndpoint.MessageHandler() {
-                public void handleMessage(String message) {
-                    System.out.println(message);
-                }
-            });
-
-            // send message to websocket
-            clientEndPoint.sendMessage("{'event':'addChannel','channel':'ok_btccny_ticker'}");
-
-            // wait 5 seconds for messages from websocket
-            Thread.sleep(5000);
-
-        } catch (InterruptedException ex) {
-            System.err.println("InterruptedException exception: " + ex.getMessage());
-        } catch (URISyntaxException ex) {
-            System.err.println("URISyntaxException exception: " + ex.getMessage());
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            String uri = "ws://localhost:9003/chat";
+            System.out.println("Connecting to " + uri);
+            container.connectToServer(MyClientEndpoint.class, URI.create(uri));
+            messageLatch.await(100, TimeUnit.SECONDS);
+        } catch (DeploymentException | InterruptedException | IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
